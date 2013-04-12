c65a50b48795efaad34c897791f8b107339e0375
Joël Kuiper <me@joelkuiper.eu>
2013-04-12 15:54:13 +0200

    Add Angular JS and Bootstrap with scaffolds

--- src/main/webapp/resources/js/indications.js
+++
@@ -1,6 +0,0 @@
-$.get(dataUrl + "/indications", function(data)  {
-	var indications = data.content;
-	$(indications).each(function(idx, indication) { 
-		$("#indications").append("<option value='" + indication.links[0].href + "'>" + indication.name + "</option>");
-	});
-});
