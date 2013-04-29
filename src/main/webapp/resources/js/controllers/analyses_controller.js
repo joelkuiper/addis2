d7bb031d768002bd9d6f0b0b39c626103d5e858d
Joël Kuiper <me@joelkuiper.eu>
2013-04-29 14:25:09 +0200

    Display study data in analyses view

--- src/main/webapp/resources/js/controllers/analyses_controller.js
+++ src/main/webapp/resources/js/controllers/analyses_controller.js
@@ -1,1 +1,23 @@
-app.controller('AnalysesController', function() {});
+app.controller('AnalysesController', function($scope, $http) {
+	$scope.measurements = [];
+	$scope.interventions = {};
+	
+	function retrieveMeasurements(data) { 
+		var populationUrl = data.population.conceptUrl; 
+		var outcomeUrls = _.pluck(data.outcomes, 'conceptUrl');
+		var interventionsUrls = _.pluck(data.interventions, 'conceptUrl');
+		
+		angular.forEach(data.interventions, function(intervention) {
+			$scope.interventions[intervention.conceptUrl] = intervention.conceptProperties;
+		});
+		
+		var baseUrl = config.dataUrl + "/measurements";
+		$http.get(baseUrl 
+				+ "?population=" + populationUrl 
+				+ "&outcome=" + outcomeUrls[0]
+				+ "&interventions=" + interventionsUrls.join())
+			.success(function(data) { $scope.measurements = data; });
+	}
+	
+	$http.get(".").success(retrieveMeasurements);
+});
